9eb22f3da002ac007306509118601ed92f3fff3e
RudyAI <rudyai@users.noreply.github.com>
2026-03-07 23:37:44 +0000

    feat: Implement gravity, jumping, and floor collision

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,16 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const JUMP_VELOCITY = -450.0
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
